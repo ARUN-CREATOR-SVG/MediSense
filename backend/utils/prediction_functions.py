167aa7f35148669125452c08c17cde1844cd26fc
Arun Singh <arunsin2212@gmail.com>
2025-06-29 10:47:38 +0530

    made common function to return prediction,prob,risklevel

--- backend/utils/prediction_functions.py
+++ backend/utils/prediction_functions.py
@@ -3,30 +3,31 @@ from utils.model_loader import load_models
 
 models=load_models()
 
-def predict_heart_disease(data:dict):
-    if 'heart' not in models:
-        raise ValueError("Heart model not loaded")
-
+def _predict_from_model(model, data: dict):
     input_df = pd.DataFrame([data])
-    prediction = models['heart'].predict(input_df)[0]
-    probability = models['heart'].predict_proba(input_df)[0][1]
+    prediction = model.predict(input_df)[0]
+    probability = model.predict_proba(input_df)[0][1]
 
     return {
         'prediction': int(prediction),
         'probability': round(float(probability), 3),
         'risk_level': 'Low' if probability < 0.4 else 'Medium' if probability < 0.7 else 'High',
     }
+
+
+def predict_heart_disease(data:dict):
+    if 'heart' not in models:
+        raise ValueError("Heart model not loaded")
+
+    return _predict_from_model(models['heart'],data)
 
 def predict_liver_disease(data:dict):
     if 'liver' not in models:
         raise ValueError("Liver model not loaded")
 
-    input_df = pd.DataFrame([data])
-    prediction = models['liver'].predict(input_df)[0]
-    probability = models['liver'].predict_proba(input_df)[0][1]
+    return _predict_from_model(models['liver'],data)
 
-    return {
-        'prediction': int(prediction),
-        'probability': round(float(probability), 3),
-        'risk_level': 'Low' if probability < 0.4 else 'Medium' if probability < 0.7 else 'High',
-    }
+def predict_diabetes_disease(data:dict):
+    if 'diabetes' not in models:
+        raise ValueError("Diabetes model not loaded")
+    return _predict_from_model(models['diabetes'],data)
