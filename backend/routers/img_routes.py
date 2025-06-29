477f8303635f86519af554653bc4146c9957dd62
Arun Singh <arunsin2212@gmail.com>
2025-06-29 15:51:07 +0530

    added route for pneumonia prediction

---
+++ backend/routers/img_routes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, UploadFile, File, status
+from fastapi.responses import JSONResponse
+from schemas.img_response  import PneumoniaPredictionResponse
+from utils.prediction_functions import predict_pneumonia_disease
+
+router = APIRouter(
+    prefix="/image",
+    tags=["Medical Image Prediction Routes"],
+)
+
+@router.post("/predict_pneumonia", response_model=PneumoniaPredictionResponse)
+async def predict_pneumonia(file: UploadFile = File(...)):
+    try:
+        result = await predict_pneumonia_disease(file)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=result)
+    except Exception as e:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content={"error": str(e)})
