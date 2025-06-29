333fcef818657e2ddfa4001f324da18c7570e9c8
Arun Singh <arunsin2212@gmail.com>
2025-06-29 14:49:57 +0530

    added pneumonia model loading code

--- backend/utils/model_loader.py
+++ backend/utils/model_loader.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import logging
+from utils.pneumonia_arch import build_pneumonia_model
 
 logger = logging.getLogger(__name__)
 
@@ -14,12 +15,20 @@ def load_models():
         'diabetes':'diabetes_model.pkl'
     }
     try:
-       for key,filename in model_files.items():
-           path=os.path.join(MODEL_DIR,filename)
-           with open(path,'rb') as f:
-               models[key]=pickle.load(f)
-               logger.info(f"{key.capitalize()} model loaded")
+        for key, filename in model_files.items():
+            path = os.path.join(MODEL_DIR, filename)
+            with open(path, 'rb') as f:
+                models[key] = pickle.load(f)
+                logger.info(f"{key.capitalize()} model loaded")
+
+        
+        pneumonia_model = build_pneumonia_model()
+        pneumonia_weights_path = os.path.join(MODEL_DIR, 'xray_model.weights.h5')
+        pneumonia_model.load_weights(pneumonia_weights_path)
+        models['pneumonia'] = pneumonia_model
+        logger.info("Pneumonia model loaded")
+
     except Exception as e:
         logger.error(f"Model Load error: {e}")
-    
+
     return models
