f13fa95cd972a62bba9852932f9ae9dd31e05b01
Arun Singh <arunsin2212@gmail.com>
2025-06-30 15:54:50 +0530

    fix: enable backend module imports without setting PYTHONPATH

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import lab_routes,img_routes
+from backend.routers import lab_routes,img_routes
 
 app=FastAPI()
 
