fa0928339cfb4a3a635d50cdcc39a2cb2a3bd696
Arun Singh <arunsin2212@gmail.com>
2025-07-07 11:47:59 +0530

    added llm router

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routers import lab_routes,img_routes
+from llm.lab_report_analysis import router as llm_router
 
 app=FastAPI()
 
@@ -28,3 +29,5 @@ def health_check():
 app.include_router(router=lab_routes.router)
 
 app.include_router(router=img_routes.router)
+
+app.include_router(router=llm_router) 
